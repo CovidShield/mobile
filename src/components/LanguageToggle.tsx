7dbbb2642ea665ad08d97c19680cf34ad1387e9a
Cândido <candidosg@gmail.com>
2020-05-23 01:50:27 -0600

    enable to choose more than two languages, fix translations

--- src/components/LanguageToggle.tsx
+++ src/components/LanguageToggle.tsx
@@ -1,15 +1,23 @@
-import React, {useCallback} from 'react';
-import {Button} from 'components/Button';
+import React from 'react';
 import {useStorage} from 'services/StorageService';
 import {useI18n} from '@shopify/react-i18n';
+import {Picker} from '@react-native-community/picker';
 
 export const LanguageToggle = () => {
   const [i18n] = useI18n();
   const {setLocale} = useStorage();
-  const toggle = useCallback(() => {
-    setLocale(i18n.locale === 'en' ? 'fr' : 'en');
-  }, [i18n.locale, setLocale]);
-  const label = i18n.locale === 'en' ? 'LanguageSelect.FrShort' : 'LanguageSelect.EnShort';
 
-  return <Button text={i18n.translate(label)} variant="subduedText" onPress={toggle} />;
+  return (
+    <Picker
+      selectedValue={i18n.locale}
+      style={{height: 50, width: 140}}
+      onValueChange={itemValue => {
+        setLocale(itemValue);
+      }}
+    >
+      <Picker.Item label={i18n.translate('LanguageSelect.EnShort')} value="en" />
+      <Picker.Item label={i18n.translate('LanguageSelect.FrShort')} value="fr" />
+      <Picker.Item label={i18n.translate('LanguageSelect.PtShort')} value="pt" />
+    </Picker>
+  );
 };

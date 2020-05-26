7830e8b169eaecc6cf6252048d431264ac080d87
Felix Glaser <leex@leex.dev>
2020-05-25 22:38:50 -0600

    Add .de

--- src/screens/language/Language.tsx
+++ src/screens/language/Language.tsx
@@ -35,7 +35,7 @@ export const LanguageScreen = () => {
   const close = useCallback(() => navigation.goBack(), [navigation]);
   const {setLocale} = useStorage();
   const toggle = useCallback(
-    (newLocale: 'en' | 'fr') => () => {
+    (newLocale: 'en' | 'fr' | 'de') => () => {
       setLocale(newLocale);
     },
     [setLocale],
@@ -67,6 +67,11 @@ export const LanguageScreen = () => {
               onPress={toggle('fr')}
               text={i18n.translate('LanguageSelect.Fr')}
               isActive={i18n.locale === 'fr'}
+            />
+            <LanguageSelectItem
+              onPress={toggle('de')}
+              text={i18n.translate('LanguageSelect.De')}
+              isActive={i18n.locale === 'de'}
             />
           </Box>
         </ScrollView>
